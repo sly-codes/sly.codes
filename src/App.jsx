21c8c306d655aae1cbd7e84f5e12bc7a8642de68
kone issa <kissadev10@gmail.com>
2025-09-25 16:48:56 +0000

    init my git

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <h1 class="text-blue-900">Hello world!</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime officia ad, quas enim
+        similique voluptas magnam odio quaerat debitis nostrum?
+      </p>
+    </>
+  );
+}
+
+export default App;
